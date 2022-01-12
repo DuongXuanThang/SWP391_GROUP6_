3356b3147009d4bfc5fa29c57be09c04531fb148
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2022-01-12 10:09:02 +0700

    dao getAllListCategory 1

--- src/java/dao/DAO.java
+++ src/java/dao/DAO.java
@@ -5,10 +5,46 @@
  */
 package dao;
 
+import context.DBContext;
+import entity.Category;
+import entity.Product;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Duong Xuan Thang
  */
 public class DAO {
-    
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+     public List<Category> getAllCategory() {
+        List<Category> list = new ArrayList<>();
+        String query = "select * from Category";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Category(rs.getInt(1),
+                        rs.getString(2)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+     public static void main(String[] args) {
+        DAO dao = new DAO();
+        
+        List<Category> listC = dao.getAllCategory();
+
+        for (Category o : listC) {
+            System.out.println(o);
+        }
+       
+    }
 }
