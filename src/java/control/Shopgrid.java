458d92936ac333edaa0c27157501420688a402c1
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2022-01-19 20:49:32 +0700

    Update Shopgrid.java

--- src/java/control/Shopgrid.java
+++ src/java/control/Shopgrid.java
@@ -5,8 +5,11 @@
  */
 package control;
 
+import dao.DAO;
+import entity.Category;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,19 +35,15 @@ public class Shopgrid extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Shopgrid</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Shopgrid at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        response.setContentType("text/html;charset=UTF-8");
+        DAO dao = new DAO();
+       
+        List<Category> listC = dao.getAllCategory();
+        request.setAttribute("listCate", listC);
+        
+        request.getRequestDispatcher("Shopgrid.jsp").forward(request, response);
         }
-    }
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
